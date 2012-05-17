3d8f182f27d4d54d636f25aca7466cf656357e8a
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-05-18 00:28:24 +0200

    making modules look preaty

--- lib/filter/twGuardGroupFormFilter.class.php
+++ lib/filter/twGuardGroupFormFilter.class.php
@@ -11,6 +11,7 @@ class twGuardGroupFormFilter extends sfGuardGroupFormFilter {
 	public function configure() {
 		unset($this['sf_guard_user_group_list']);
 		$this->widgetSchema['sf_guard_group_permission_list']->setLabel('Permissions');
+		$this->setWidget('name', new sfWidgetFormFilterInput(array('with_empty' => false, 'template' => '%input%&nbsp;%empty_checkbox%&nbsp;%empty_label%')));
 		$this->setWidget('description', new sfWidgetFormFilterInput(array('template' => '%input%&nbsp;%empty_checkbox%&nbsp;%empty_label%')));
 	}
 }
