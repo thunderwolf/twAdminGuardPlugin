35e6278a3833b96e81dc836bd8901ba6e1b478bc
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-05-17 22:45:42 +0200

    rename - bugfix

--- modules/twGuardPermission/lib/BasetwGuardPermissionActions.class.php
+++ modules/twGuardPermission/lib/BasetwGuardPermissionActions.class.php
@@ -9,7 +9,7 @@ require_once dirname(__FILE__).'/twGuardPermissionGeneratorHelper.class.php';
  * @package    sfGuardPlugin
  * @subpackage sfGuardPermission
  * @author     Fabien Potencier
- * @version    SVN: $Id: BasetwGuardPermissionActions.class.php 3556 2012-05-05 14:48:10Z ldath $
+ * @version    SVN: $Id: BasetwGuardPermissionActions.class.php 3564 2012-05-17 20:44:56Z ldath $
  */
 class BasetwGuardPermissionActions extends autotwGuardPermissionActions
 {
--- modules/twGuardPermission/lib/twGuardPermissionGeneratorConfiguration.class.php
+++ modules/twGuardPermission/lib/twGuardPermissionGeneratorConfiguration.class.php
@@ -6,7 +6,7 @@
  * @package    sfGuardPlugin
  * @subpackage sfGuardPermission
  * @author     Fabien Potencier
- * @version    SVN: $Id: twGuardPermissionGeneratorConfiguration.class.php 3556 2012-05-05 14:48:10Z ldath $
+ * @version    SVN: $Id: twGuardPermissionGeneratorConfiguration.class.php 3564 2012-05-17 20:44:56Z ldath $
  */
 class twGuardPermissionGeneratorConfiguration extends BaseTwGuardPermissionGeneratorConfiguration
 {
