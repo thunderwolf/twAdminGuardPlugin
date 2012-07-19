da4e1e65842b77d1a6525ac6fe12808dfa7b3c37
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-07-20 01:15:22 +0200

    work on documentation

--- modules/twGuardPermission/lib/BasetwGuardPermissionActions.class.php
+++ modules/twGuardPermission/lib/BasetwGuardPermissionActions.class.php
@@ -9,7 +9,6 @@ require_once dirname(__FILE__).'/twGuardPermissionGeneratorHelper.class.php';
  * @package    sfGuardPlugin
  * @subpackage sfGuardPermission
  * @author     Fabien Potencier
- * @version    SVN: $Id: BasetwGuardPermissionActions.class.php 3564 2012-05-17 20:44:56Z ldath $
  */
 class BasetwGuardPermissionActions extends autotwGuardPermissionActions
 {
--- modules/twGuardPermission/lib/twGuardPermissionGeneratorConfiguration.class.php
+++ modules/twGuardPermission/lib/twGuardPermissionGeneratorConfiguration.class.php
@@ -6,7 +6,6 @@
  * @package    sfGuardPlugin
  * @subpackage sfGuardPermission
  * @author     Fabien Potencier
- * @version    SVN: $Id: twGuardPermissionGeneratorConfiguration.class.php 3564 2012-05-17 20:44:56Z ldath $
  */
 class twGuardPermissionGeneratorConfiguration extends BaseTwGuardPermissionGeneratorConfiguration
 {
