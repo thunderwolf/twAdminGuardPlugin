4ff1e76e3f9c1367173cf880f0983b49357ccc77
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2012-09-18 21:52:40 +0200

    simplify

--- lib/filter/twGuardUserFormFilter.class.php
+++ lib/filter/twGuardUserFormFilter.class.php
@@ -10,11 +10,6 @@
 class twGuardUserFormFilter extends sfGuardUserFormFilter {
 	public function configure() {
 		unset($this['algorithm'], $this['salt'], $this['password']);
-		
-		$this->widgetSchema['username'] = new sfWidgetFormFilterInput(array(
-			'with_empty' => false, 'template' => '%input% %empty_checkbox% %empty_label%'
-		));
-		
 		$this->widgetSchema['sf_guard_user_group_list']->setLabel('Groups');
 		$this->widgetSchema['sf_guard_user_permission_list']->setLabel('Permissions');
 	}
