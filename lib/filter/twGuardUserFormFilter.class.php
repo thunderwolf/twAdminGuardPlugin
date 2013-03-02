cab5fb912dcbe6c3797369f3a90bfd68206e6eb7
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-03-02 22:31:29 +0100

    integration with new twAdmin

--- lib/filter/twGuardUserFormFilter.class.php
+++ lib/filter/twGuardUserFormFilter.class.php
@@ -12,5 +12,13 @@ class twGuardUserFormFilter extends sfGuardUserFormFilter {
 		unset($this['algorithm'], $this['salt'], $this['password']);
 		$this->widgetSchema['sf_guard_user_group_list']->setLabel('Groups');
 		$this->widgetSchema['sf_guard_user_permission_list']->setLabel('Permissions');
+		$this->getWidget('last_login')
+			->setOption('from_date', new sfWidgetFormInput(array(), array('class' => 'input-large j-datetimepicker')))
+			->setOption('to_date', new sfWidgetFormInput(array(), array('class' => 'input-large j-datetimepicker')))
+			->setOption('template', 'from<br />%from_date%<br />to<br />%to_date%')
+			->setOption('filter_template', '%date_range%<br />%empty_checkbox%&nbsp;%empty_label%');
+		$this->getValidator('last_login')
+			->setOption('from_date', new sfValidatorDateTime(array('required' => false)))
+			->setOption('to_date', new sfValidatorDateTime(array('required' => false)));
 	}
 }
